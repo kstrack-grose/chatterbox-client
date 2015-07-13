137fc87d70795577b37b3d442e846e1e135ba928
kstrack-grose <kiristrackgrose@gmail.com>
2015-07-13 16:06:21 -0700

    initialize html, css, and script: can find information in text input

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,32 @@
-// YOUR CODE HERE:
+var app = {};
+
+app.init = function(){
+
+};
+
+app.send = function(message){ 
+  var $outMessage = $(".message");
+  $($outMessage).submit(function(event) {
+    event.preventDefault();
+  });
+  var $outMessageData = $($outMessage).serialize();
+  debugger;
+  // actually post the message with ajax
+  $.ajax({
+    url: "https://api.parse.com/1/classes/chatterbox",
+    type: "POST",
+    data: $outMessageData
+  });
+};
+
+
+
+// view messages
+
+// select a username and input messages
+
+// refresh (auto or with a button)
+
+//
+
 
