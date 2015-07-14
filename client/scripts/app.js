e9f2ed4aa7e30d2dba288b4d33e2eabce6726936
kstrack-grose <kiristrackgrose@gmail.com>
2015-07-13 17:17:14 -0700

    small edits added; up to events section

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,10 +1,17 @@
 var app = {};
 app.server = "https://api.parse.com/1/classes/chatterbox";
 
+// user object contains references to all of the things about YOU
+// the user
+app.user = {
+  username: "username",
+  friends: []
+}
+// INIT. For currently unknown reasons.
 app.init = function(){
 
 };
-
+////////// MANIPULATE MESSAGES ///////////
 // send message
 app.send = function(message){ 
   // var $outMessage = $(".message");
@@ -20,7 +27,6 @@ app.send = function(message){
     data: message
   });
 };
-
 // view messages
 app.fetch = function() {
   $.ajax({
@@ -28,20 +34,44 @@ app.fetch = function() {
     type: "GET"
   });
 };
-
+//////// ALTERING THE DOM /////////
 // clear messages from the DOM
 app.clearMessages = function() {
   $("#chats").empty();
 };
-
 // add messages to the DOM
 app.addMessage = function(message) {
-  var newMessage = "<div class='message'><span class='username'>message.username</span><span class='text'>message.text</span></div>";
+  var newMessage = "<div class='message'><span><button class='username'>" + message.username + "</button></span><span class='text'>" + message.text + "</span></div>";
   $("#chats").append(newMessage);
 };
-
+// add rooms to select menu
 app.addRoom = function(roomName) {
   $('#roomSelect').append($('<option>', {
     text: roomName
   }));
 };
+/////// EVENTS METHODS ///////////
+// add a friend by clicking their username
+app.addFriend = function(friendName) {
+  user.friends.push(friendName);
+};
+
+
+
+//JQUERY
+$("document").ready(function() {
+  $(".username").on("click", function() {
+    app.addFriend(this.text);
+  });
+
+
+}); //end document.ready
+
+
+
+
+
+
+
+
+
